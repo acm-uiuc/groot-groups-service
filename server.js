85fc8e164d0fcdbcf3895b89e409ac17029078f0
Ish Shah <ishshah2@illinois.edu>
2016-10-29 18:21:46 -0500

    not ugly line

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-var app=require("express")(),path=require("path");app.get("/sigs",function(a,b){b.setHeader("Content-Type","application/json"),b.sendFile(path.resolve(__dirname)+"/sigs.json")}),app.listen(process.env.PORT||8e3,function(){var a=this.address().port;console.log("Server running on",a)});
+var app = require("express")(),
+    path = require("path");
+app.get("/sigs", function(a, b) {
+    b.setHeader("Content-Type", "application/json"), b.sendFile(path.resolve(__dirname) + "/sigs.json")
+}), app.listen(process.env.PORT || 9001, function() {
+    var a = this.address().port;
+    console.log("Server running on", a)
+});
