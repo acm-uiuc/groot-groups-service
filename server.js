ef067802521454dbdef322bf2e6a8f9f1cb3b77d
Benjamin Congdon <bencongdon96@gmail.com>
2017-01-19 13:13:25 -0600

    Add winston-express logging

--- server.js
+++ server.js
@@ -10,12 +10,21 @@
 
 var express = require('express');
 var path = require("path");
+const winston = require('winston');
+const expressWinston = require('express-winston');
 
 var app  = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
+}));
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  meta: false, // don't log metadata about requests (produces very messy logs if true)
+  expressFormat: true, // Use the default Express/morgan request formatting.
 }));
 
 var request = require('request');
